8e2a612b65259d280dc78fbfcbe4f9c81b53b089
panchicore <thepanchi@gmail.com>
2011-02-20 20:59:01 -0500

    more params for pywebshots

--- screenshots/models.py
+++ screenshots/models.py
@@ -1,23 +1,23 @@
-from django_extensions.db.fields import CreationDateTimeField
+from django_extensions.db.fields import CreationDateTimeField, UUIDField
 from django.db import models
 from fabric.api import local
 from django.conf import settings
 import os
 
 class Screenshot(models.Model):
+    uuid = UUIDField()
     image = models.ImageField(upload_to='screenshots', null = True, blank = True)
     url = models.URLField(verify_exists=False)
     created = CreationDateTimeField()
 
-    def save_screenshot(self, delay = 3):
-        screenshot = os.path.join("screenshots", "%i.png" % self.id)
+    def save_screenshot(self, size='200x200', delay = 3):
+        screenshot = os.path.join("screenshots", "%s.png" % self.uuid)
         screenshot_path = os.path.join(settings.MEDIA_ROOT, screenshot)
-        a = local('xvfb-run -s "-screen 0 1024x768x24" %s -d %i -t 200x200 %s -f %s' %  (
-                                                                                        settings.PYWEBSHOT_PATH,
-                                                                                        delay,
-                                                                                        self.url,
-                                                                                        screenshot_path
-                                                                                        ))
+        a = local('xvfb-run -s "-screen 0 1024x768x24" %s -d %i -t %s %s -f %s' %  (
+                                                                                    settings.PYWEBSHOT_PATH,
+                                                                                    delay, size, self.url,
+                                                                                    screenshot_path
+                                                                                    ))
         self.image = screenshot
         self.save()
 
